7ebc39b95058137cb822c5742080d7dad359ac35
mtkane6 <mtkane6@gmail.com>
2019-12-23 21:53:52 -0800

    Included notes about moving average solution.

--- MovingAverageFromDataStream.py
+++ MovingAverageFromDataStream.py
@@ -2,7 +2,15 @@
 Runtime: 104 ms, faster than 15.33% of Python online submissions for Moving Average from Data Stream.
 Memory Usage: 15.1 MB, less than 60.00% of Python online submissions for Moving Average from Data Stream.
 
+Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
 
+Example:
+
+MovingAverage m = new MovingAverage(3);
+m.next(1) = 1
+m.next(10) = (1 + 10) / 2
+m.next(3) = (1 + 10 + 3) / 3
+m.next(5) = (10 + 3 + 5) / 3
 
 '''
 
